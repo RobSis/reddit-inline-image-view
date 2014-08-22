76956fa0ec73efdeb6fa0c98b7c78fe25386ef0d
Robert Siska <robert.siska@magnolia-cms.com>
2014-08-22 14:45:41 +0200

    Synced expando buttons with view image button. Comments work with minor issue.

--- reddit-inline-image-view.user.js
+++ reddit-inline-image-view.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           reddit-inline-image-view
-// @namespace      http://github.com/RobSis/
+// @namespace      reddit_image_expando
 // @updateURL      https://github.com/RobSis/reddit-inline-image-view/blob/master/reddit-inline-image-view.user.js
 // @description    Shows directly linked images or imgur images in the current page.
 // @version        1.0
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 /*
- * Based on script by /u/drowsap: http://www.reddit.com/r/AskReddit/comments/9ydts/c0f0ltr
+ * Based on script by /u/drowsap (http://www.reddit.com/r/AskReddit/comments/9ydts/c0f0ltr)
  * and drag-resizing code from honestbleep's Reddit Enhancement Suite.
  *
  *
@@ -27,7 +27,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var imageData = Array();
+
+var BUTTON_EXPANDED_CLASS = 'expando-button expanded selftext rixButton';
+var BUTTON_COLLAPSED_CLASS = 'expando-button collapsed selftext rixButton';
+
+var BUTTON_COMMENT_STYLE = "vertical-align:top !important; float: none; width: 23px; height: 23px;" +
+"max-width: 23px; max-height: 23px; display: inline-block;" +
+"margin-right: 6px; cursor: pointer;  padding: 0px;"
+
+var imageData = [];
 
 /*
  * Calculate the drag size for the event.
@@ -37,7 +45,7 @@ var imageData = Array();
  */
 function getDragSize(e)
 {
-    return (p = Math.pow)(p(e.clientX - (rc = e.target.getBoundingClientRect()).left, 2) + p(e.clientY - rc.top, 2), .5);
+    return (p = Math.pow)(Math.pow(e.clientX - (rc = e.target.getBoundingClientRect()).left, 2) + Math.pow(e.clientY - rc.top, 2), 0.5);
 }
 
 /*
@@ -52,91 +60,6 @@ function getHeight()
 }
 
 /*
- * Function mapped to 'View images' button.
- */
-function viewImages(find_string)
-{
-    var x = $(".content").find(find_string).each(function()
-    {
-        viewImage($(this));
-    });
-};
-
-/*
- * Expand the image link specified by given 'a' element.
- * (Add 'img' element after it)
- */
-function viewImage(a)
-{
-    if ((a.hasClass("rexImage")) && (a.next(".rexMorphed").length == 0))
-    {
-        // add .jpg extension to imgur links without one
-        var href=a.attr("href");
-        var ext = (href.indexOf('imgur.') >= 0 && href.indexOf('.jpg') < 0 && href.indexOf('.png') < 0 && href.indexOf('.gif') < 0) ? '.jpg' : '';
-
-        var img = document.createElement('img');
-        img.setAttribute('class', 'rexMorphed');
-        img.setAttribute('style', 'display:block;max-width:500px;');  // TODO: get width of main content div?
-        img.setAttribute('src', href+ext);
-
-        imageData[img] = {
-            zindex: img.style.zIndex,
-            width: img.style.width,
-            height: img.style.height,
-            position: img.style.position,
-            resized: 0,
-            resizable: true
-        }
-        makeImageZoomable(img);
-        a.parent().parent().append(img);
-    }
-}
-
-/*
- * Collapse the image link specified by given 'a' element.
- * (Remove 'img' element after it)
- */
-function hideImage(a)
-{
-    a.parent().parent().find(".rexMorphed").remove();
-}
-
-/*
- * Inject expand buttons to any image that can be expanded.
- */
-function injectExpandos(find_string, comments)
-{
-    var x = $(".content").find(find_string).each(function()
-    {
-        var a=$(this).find("a")
-        if (!comments)
-        {
-            a=$(this).find("a.title")
-        }
-        var href=a.attr("href");
-        var title_text=a.text();
-        if ((!a.hasClass("rexMorphed")) && (a.next(".rexMorphed").length == 0) && href &&
-                (href.indexOf('imgur.') >= 0 || href.indexOf('.jpeg') >= 0 || href.indexOf('.jpg') >= 0 || href.indexOf('.gif') >= 0 || href.indexOf('.png') >= 0))
-        {
-            a.addClass('rexImage');
-            var theDiv = document.createElement("div");
-            theDiv.setAttribute("class", "expando-button collapsed selftext");
-            theDiv.addEventListener("click", function()
-                {
-                    if (theDiv.className.indexOf("collapsed") > 0) {
-                        theDiv.setAttribute("class", "expando-button expanded selftext");
-                        viewImage(a);
-                    } else {
-                        theDiv.setAttribute("class", "expando-button collapsed selftext");
-                        hideImage(a);
-                    }
-                });
-            a.parent().after(theDiv);
-        }
-    });
-}
-
-/*
  * Set up events for the given img element to make it zoomable via
  * drag to zoom. Most of this is taken directly from honestbleeps's
  * Reddit Enhancement Suite. Event functions are currently written
@@ -147,7 +70,7 @@ function injectExpandos(find_string, comments)
  */
 function makeImageZoomable(imgTag)
 {
-    DragData = {};
+    var DragData = {};
 
     imgTag.addEventListener('mousedown', function(e)
     {
@@ -200,10 +123,10 @@ function makeImageZoomable(imgTag)
     {
         if (DragData.dragging)
         {
-            clingdelta = Math.abs(DragData.delta - getDragSize(e));
+            var clingdelta = Math.abs(DragData.delta - getDragSize(e));
             if (clingdelta > 5)
             {
-                var prevwidth = parseInt(e.target.style.width.replace('px', ''));
+                var prevwidth = parseInt(e.target.style.width.replace('px', ''), 10);
 
                 e.target.style.maxWidth = e.target.style.width = Math.floor(((getDragSize(e)) * DragData.width / DragData.delta)) + "px";
                 e.target.style.maxHeight = '';
@@ -215,7 +138,7 @@ function makeImageZoomable(imgTag)
                     e.target.style.position = 'relative';
                 }
 
-                imageData[e.target].resized = (prevwidth - parseInt(e.target.style.width.replace('px', '')));
+                imageData[e.target].resized = (prevwidth - parseInt(e.target.style.width.replace('px', ''), 10));
             }
         }
     }, false);
@@ -251,9 +174,6 @@ function makeImageZoomable(imgTag)
         if (e.metaKey != null && e.metaKey != 0)  // Can happen on some platforms
             return true;
 
-        //console.log("Click [click]: "+e.button);
-        //console.log("Resize [click]: "+imageData[e.target].resized);
-
         if (!isNaN(imageData[e.target].resized) && imageData[e.target].resized != 0)
         {
             e.preventDefault();
@@ -263,32 +183,202 @@ function makeImageZoomable(imgTag)
     }, true);
 }
 
+/*
+ * Expand the image link specified by given 'a' element.
+ * (Add 'img' element after it)
+ */
+function viewImage(a, comments)
+{
+    if (a.hasClass('rixImageLink') && a.hasClass('collapsed'))
+    {
+        // add .jpg extension to imgur links without one
+        var href=a.attr('href');
+        var ext = (href.indexOf('imgur.') >= 0 && href.indexOf('.jpg') < 0 && href.indexOf('.png') < 0 && href.indexOf('.gif') < 0) ? '.jpg' : '';
+
+        var img = document.createElement('img');
+        a.attr('class', 'rixImageLink expanded');
+        img.setAttribute('class', 'rixExpandedImage');
+        img.setAttribute('style', 'display:block;max-width:500px;');  // TODO: get width of main content div?
+        img.setAttribute('src', href+ext);
+
+        imageData[img] = {
+            zindex: img.style.zIndex,
+            width: img.style.width,
+            height: img.style.height,
+            position: img.style.position,
+            resized: 0,
+            resizable: true
+        };
+        makeImageZoomable(img);
+        if (comments)
+        {
+            a.next(".rixButton").after(img);
+        } else
+        {   
+        	a.parent().parent().append(img);
+        }
+    }
+}
+
+/*
+ * Collapse the image link specified by given 'a' element.
+ * (Remove 'img' element after it)
+ */
+function hideImage(a)
+{
+    a.attr('class', 'rixImageLink collapsed');
+    a.parent().parent().find('.rixExpandedImage').remove();
+}
+
+/*
+ * Function mapped to 'View images' button.
+ */
+function viewImages(find_string, comments)
+{
+    var x = $('.content').find(find_string).each(function()
+    {
+        viewImage($(this), comments);
+    });
+}
+
+/*
+ * Function mapped to 'Hide images' button.
+ */
+function hideImages(find_string)
+{
+    var x = $('.content').find(find_string).each(function()
+    {
+        hideImage($(this));
+    });
+}
+
+/*
+ * Inject expand button to any image that can be expanded.
+ */
+function injectExpandos(find_string, comments)
+{
+    var imagesFound = 0;
+    var x = $('.content').find(find_string).each(function()
+    {
+        var a=$(this);
+        var href=a.attr('href');
+        var title_text=a.text();
+        if (!a.hasClass('.rixImageLink') && href &&    // not already injected links 
+                ((href.indexOf('imgur.') >= 0 && href.indexOf('/a/') < 0 && href.indexOf('/gallery/') < 0) || // no albums and galleries
+                    href.indexOf('.jpeg') >= 0 || href.indexOf('.jpg') >= 0 || href.indexOf('.gif') >= 0 || href.indexOf('.png') >= 0))
+        {
+            a.attr('class', 'rixImageLink collapsed');
+            imagesFound += 1;
+
+            var theDiv = document.createElement('div');
+            theDiv.setAttribute('class', BUTTON_COLLAPSED_CLASS);
+            theDiv.addEventListener('click', function()
+                {
+                    if (theDiv.className.indexOf('collapsed') > 0)
+                    {
+                        theDiv.setAttribute('class', BUTTON_EXPANDED_CLASS);
+                        viewImage(a, comments);
+                    } else
+                    {
+                        theDiv.setAttribute('class', BUTTON_COLLAPSED_CLASS);
+                        hideImage(a);
+                    }
+                }, true);
+
+            if (comments)
+            {
+                theDiv.setAttribute('style', BUTTON_COMMENT_STYLE);
+                a.after(theDiv);
+            } else
+            {
+                a.parent().after(theDiv);
+            }
+        }
+    });
+    return imagesFound;
+}
+
+/*
+ * Change all buttons to expanded.
+ */
+function expandButtons(find_string)
+{
+    var x = $('.content').find(find_string).each(function()
+    {
+        if ($(this).hasClass('collapsed'))
+        {
+            $(this).attr('class', BUTTON_EXPANDED_CLASS);
+        }
+    });
+}
+
+/*
+ * Change all buttons to collapsed.
+ */
+function collapseButtons(find_string)
+{
+    var x = $('.content').find(find_string).each(function()
+    {
+        if ($(this).hasClass('expanded'))
+        {
+            $(this).attr('class', BUTTON_COLLAPSED_CLASS);
+        }
+    });
+}
+
 // Main entry
 (function()
 {
-    injectExpandos("#siteTable div.entry", false);
-    injectExpandos(".usertext-body .md", true);
+    var imagesFound = 0;
+    imagesFound += injectExpandos('#siteTable div.entry p.title a.title', false);
+    imagesFound += injectExpandos('.usertext-body .md p a', true);
+    var numberOfImages = ' (' + imagesFound + ')';
 
     var header = document.getElementById('header-bottom-left');
     var uls = header.getElementsByTagName('ul');
 
+    var tabmenu = null;
     for (var i = 0; i < uls.length; i++)
     {
         if (uls[i].className=='tabmenu ')
         {
-            var li = document.createElement('li');
-            var a = document.createElement('a');
-            var text = document.createTextNode('view images');
-
-            a.setAttribute('href','javascript:void(0);');  // just for arrow cursor
-            a.addEventListener("click", function() {
-                viewImages("#siteTable div.entry p.title a.title");  // topics
-                viewImages(".usertext-body div.md p a");  // comment section
-            }, false);
-            a.appendChild(text);
-            li.appendChild(a);
-            uls[i].appendChild(li);
+            tabmenu = uls[i];
         }
     }
-    document.addEventListener('dragstart', function() {return false}, false);
+
+    if (tabmenu != null)
+    {
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        var text = document.createTextNode('view images' + numberOfImages);
+
+        a.setAttribute('class', 'rixViewAll');
+        a.setAttribute('href', 'javascript:void(0);');  // just to get an arrow cursor..
+        a.addEventListener('click', function()
+            {
+                if (a.classList.contains('rixViewAll'))
+                {
+                    a.setAttribute('class', 'rixHideAll');
+                    text.nodeValue='hide images' + numberOfImages;
+
+                    viewImages('#siteTable div.entry p.title a', false);  // topics
+                    viewImages('.usertext-body div.md p a', true);  // comment section
+
+                    expandButtons('.rixButton');
+                } else
+                {
+                    a.setAttribute('class', 'rixViewAll');
+                    text.nodeValue='view images' + numberOfImages;
+
+                    hideImages('#siteTable div.entry p.title a');
+                    hideImages('.usertext-body div.md p a');
+
+                    collapseButtons('.rixButton');
+                }
+            }, false);
+        a.appendChild(text);
+        li.appendChild(a);
+        tabmenu.appendChild(li);
+    }
+    document.addEventListener('dragstart', function() {return false;}, false);
 })();
